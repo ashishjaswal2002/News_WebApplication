e2e1faeff9fc34a1b10b249329cc5e38e41bb40e
Ashish jaswal <jaswalashish131@gmail.com>
2023-05-26 02:12:29 +0530

    done

--- next.config.js
+++ next.config.js
@@ -3,13 +3,7 @@ const nextConfig = {
     env:{
         secretKeyTech:'bb4984e470468da6c24fb9583bf54eab'
     },
-    images: {
-        formats: ['image/webp'],
-        domains: ['feeds.abplive.com'],
-        domains: ['www.hindustantimes.com'],
-       
-
-    },
+    
    
 }
 
